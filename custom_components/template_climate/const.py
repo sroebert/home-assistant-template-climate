e2c8b27d384538f30de342a181851bdf298a62f2
Steven Roebert <steven@roebert.nl>
2025-10-04 21:13:02 +0000

    Setup of template climate custom component

---
+++ custom_components/template_climate/const.py
@@ -0,0 +1,8 @@
+"""Constants for template_climate."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "template_climate"
+DEFAULT_NAME = "Template Climate"
